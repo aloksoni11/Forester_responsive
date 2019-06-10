003d621c084c7dd81a5b8d7f11ba7ef63029f4ac
aloksoni11 <sonialok5777@gmail.com>
2019-06-10 10:55:38 +0530

    update

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ window.onscroll = function () {
 };
 
 function myFunction() {
+    var Img = document.getElementById("brand-logo");
+    var x = document.getElementsByClassName("menu-a");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("navbar").classList.add("navview");
         document.getElementById("menu1").classList.add("text-dark");
-        var x = document.getElementsByClassName("menu-a");
+        Img.src = "images/logo-dark.png";
         for (var i = 0; i < x.length; i++) {
             x[i].classList.add("text-dark");
         }
@@ -14,7 +16,7 @@ function myFunction() {
     else {
         document.getElementById("navbar").classList.remove("navview");
         document.getElementById("menu1").classList.remove("text-dark");
-        var x = document.getElementsByClassName("menu-a");
+        Img.src = "images/logo.png";
         for (var i = 0; i < x.length; i++) {
             x[i].classList.remove("text-dark");
         }
@@ -22,18 +24,25 @@ function myFunction() {
 }
 $(document).ready(function () {
     $("#lightgallery").lightGallery();
+    $(document).ready(function () {
+        $(".owl-carousel").owlCarousel();
+    });
 });
-
 $(window).scroll(startCounter);
+
 function startCounter() {
     if ($(window).scrollTop() > 3500) {
         $(window).off("scroll", startCounter);
         $('.count').each(function () {
             var $this = $(this);
-            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                duration: 2000,
-                easing: 'swing',
-                step: function () {
+            jQuery({
+                Counter: 0
+            }).animate({
+                Counter: $this.text()
+            }, {
+                duration: 1000
+                , easing: 'swing'
+                , step: function () {
                     $this.text(Math.ceil(this.Counter));
                 }
             });
